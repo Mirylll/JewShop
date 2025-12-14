c1fcbb6fc35d997876d532366263999b28ea2e38
Miryl <hntb0102@gmail.com>
2025-12-14 22:33:49 +0700

    Update ApplicationDbContext

--- JewShop.Server/Data/ApplicationDbContext.cs
+++ JewShop.Server/Data/ApplicationDbContext.cs
@@ -10,11 +10,8 @@ namespace JewShop.Server.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
-<<<<<<< HEAD
         public DbSet<User> Users { get; set; }       // Mới thêm
         public DbSet<Session> Sessions { get; set; } // Mới thêm
-=======
         public DbSet<Coupon> Coupons { get; set; }
->>>>>>> phanquy
     }
 }
